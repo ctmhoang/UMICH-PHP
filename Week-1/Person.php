cbfaf0aceabb0e87e32a60b641c63b43b3ad49cd
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-02-01 05:17:18 -0500

    Extends from one class to another

--- Week-1/Person.php
+++ Week-1/Person.php
@@ -18,7 +18,7 @@ class Person
 
     public function greet()
     {
-        return "Hello $this->firstname $this->lastname Your age is $this->age";
+        return "Hello $this->firstname $this->lastname. Your age is $this->age";
     }
 
     public static function ahoy()
@@ -32,7 +32,34 @@ class Person
     }
 }
 
+
+class Curmudgeon extends Person
+{
+    private string $lang = "";
+
+    function __construct(string $lastname, string $firstname, int $age, string $lang)
+    {
+        parent::__construct($lastname, $firstname, $age);
+        $this->lang = $lang;
+    }
+    function bye()
+    {
+        if ($this->lang == 'fr') {
+            echo 'Au revoir';
+            return;
+        }
+        if ($this->lang == 'es') {
+            echo 'Adios';
+            return;
+        }
+        echo 'goodbye';
+    }
+}
+
 $cam = new Person('Hoang', 'Cam', 21);
 $cam->greet();
 Person::ahoy();
 echo (Person::CLASS_NAME);
+
+$er = new Curmudgeon("Eric","William",67,'fr');
+$er->bye();
