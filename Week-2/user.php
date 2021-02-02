4a74934cea43ddf5e3dbc687c88e2dd910ec6df2
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-02-02 04:11:58 -0500

    Finish CRUD example

--- Week-2/user.php
+++ Week-2/user.php
@@ -1,5 +1,18 @@
 <?php
 require_once "pdo.php";
+
+if (isset($_POST['name'], $_POST['email'], $_POST['pass'])) {
+    $sql = 'insert into users (name,email,password) values (:name, :email, :password)';
+    echo "<pre>$sql</pre>";
+    $stm = $pdo->prepare($sql);
+    $stm->execute([':name' => $_POST['name'], ':email' => $_POST['email'], ':password' => $_POST['pass']]);
+    echo 'Add successfully';
+} else if (isset($_POST['delete'], $_POST['u_id'])) {
+    $sql = 'delete from users where user_id = :uid';
+    echo "<pre>$sql</pre>";
+    $stm = $pdo->prepare($sql);
+    $stm->execute(['uid' => $_POST['u_id']]);
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,6 +21,13 @@ require_once "pdo.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users CRUD</title>
+    <style>
+        label {
+            display: block
+        }
+
+        ;
+    </style>
 </head>
 
 <body>
@@ -35,7 +55,13 @@ require_once "pdo.php";
             ?>
         </tr>
     </table>
-
+    <form method="post">
+        <p>Add a new users</p>
+        <label for="name">Name: <input type="text" name="name"></label>
+        <label for="email">Email: <input type="email" name="email"></label>
+        <label for="pass">Passwords: <input type="password" name="pass"></label>
+        <button>Add</button>
+    </form>
 </body>
 
 </html>
